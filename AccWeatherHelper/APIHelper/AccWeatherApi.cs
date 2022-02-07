a61a955e8a358e27c03a8ea4a3ac05a3ab4bcf64
Kuntal Das <25ckuntaldas@gmail.com>
2022-02-07 20:13:07 +0530

    AccWeather api helpers

---
+++ AccWeatherHelper/APIHelper/AccWeatherApi.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Extensions.Configuration.UserSecrets;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TheWeatherApp.Models;
+
+namespace AccWeatherHelper.APIHelper
+{
+    public class AccWeatherApi
+    {
+        private static readonly string API_KEY = Environment.GetEnvironmentVariable("AccWeather:KEY");
+        private static readonly string BaseUrl = @"http://dataservice.accuweather.com/";
+        private static string AutoCompleteEndPoint = @"locations/v1/cities/autocomplete/apikey={0}&Q={1}";
+        private static string CurrentConditionsEndPoint = @"currentconditions/v1/{0}/apikey={1}";
+
+        public static async Task<List<City>> GetCitiesAsync(string query)
+        {
+            List<City> cities = new();
+
+            string url = $"{BaseUrl}{string.Format(AutoCompleteEndPoint, API_KEY, query)}";
+
+            using (HttpClient httpClient = new())
+            {
+                var response = await httpClient.GetAsync(url);
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+
+        public static async Task<CurrentConditions> GetCurrentConditionsAsync(string citykey)
+        {
+            CurrentConditions currentConditions = new();
+
+            string url = $"{BaseUrl}{string.Format(CurrentConditionsEndPoint, citykey, API_KEY)}";
+
+            using (HttpClient httpClient = new())
+            {
+                var response = await httpClient.GetAsync(url);
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                currentConditions = JsonConvert
+                    .DeserializeObject<List<CurrentConditions>>(json)
+                    .FirstOrDefault();
+            }
+
+            return currentConditions;
+        }
+    }
+}
