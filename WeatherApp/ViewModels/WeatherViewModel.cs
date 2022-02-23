7798fc704dbff1e5ac64f3adb5c9f834247fac54
Kuntal Das <25ckuntaldas@gmail.com>
2022-02-23 20:41:30 +0530

    Weather View model

--- WeatherApp/ViewModels/WeatherViewModel.cs
+++ WeatherApp/ViewModels/WeatherViewModel.cs
@@ -1,11 +1,16 @@
-﻿using AccWeatherHelper.Models;
+﻿using AccWeatherHelper.APIHelper;
+using AccWeatherHelper.Models;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using WeatherApp.Commands;
 
 namespace WeatherApp.ViewModels
 {
     public class WeatherViewModel : INotifyPropertyChanged
     {
+        public ObservableCollection<City> Cities { get; set; }
         private string _cityQuery;
 
         public string CityQuery
@@ -39,20 +44,37 @@ namespace WeatherApp.ViewModels
             {
                 _selectedCity = value;
                 OnPropChange();
+                GetCurrentConditionsAsync();
             }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+        public SearchCityCommand SearchCityCommand { get; set; }
+
+        public WeatherViewModel()
+        {
+            SearchCityCommand = new SearchCityCommand(this);
+            Cities = new ObservableCollection<City>();
+        }
+
         private void OnPropChange([CallerMemberName] string propName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
         }
 
-        public WeatherViewModel()
+        public async Task SearchCityAsync()
         {
-            
+            var cities = await AccWeatherApi.GetCitiesAsync(CityQuery);
+
+            Cities.Clear();
+            foreach (var city in cities) Cities.Add(city);
         }
 
-
+        private async Task GetCurrentConditionsAsync()
+        {
+            CityQuery = string.Empty;
+            Cities.Clear();
+            CurrentConditions = await AccWeatherApi.GetCurrentConditionsAsync(SelectedCity.Key);
+        }
     }
 }
