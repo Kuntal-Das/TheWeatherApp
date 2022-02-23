cbb6cf9d8706aefcfddcddf06858ed0db68f5fac
Kuntal Das <25ckuntaldas@gmail.com>
2022-02-23 20:42:02 +0530

    converter which is not used

---
+++ WeatherApp/Converters/BoolToRainConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherApp.Converters
+{
+    class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value)
+                ? "Currently Raining"
+                : "Currently Not Raining";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as string).Equals("Currently Raining");
+        }
+    }
+}
