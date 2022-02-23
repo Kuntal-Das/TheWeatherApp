8828d1d012944194e8a8e8d5da11f48fe716c15c
Kuntal Das <25ckuntaldas@gmail.com>
2022-02-23 20:40:44 +0530

    Search City Command

---
+++ WeatherApp/Commands/SearchCityCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WeatherApp.ViewModels;
+
+namespace WeatherApp.Commands
+{
+    public class SearchCityCommand : ICommand
+    {
+        public WeatherViewModel VM { get; set; }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public SearchCityCommand(WeatherViewModel weatherViewModel)
+        {
+            VM = weatherViewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (!string.IsNullOrWhiteSpace(parameter as string)) && (parameter as string).Length > 3;
+        }
+
+        public void Execute(object parameter)
+        {
+            _ = VM.SearchCityAsync();
+        }
+    }
+}
